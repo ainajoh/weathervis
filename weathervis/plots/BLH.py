b5c3923ed791076f3234c5867abf84be525ab833
ainajoh <aina.johannessen@gmail.com>
2021-09-22 00:00:33 +0200

    BLH included

---
+++ weathervis/plots/BLH.py
@@ -0,0 +1,87 @@
+# %%
+#python BLH.py --datetime 2020091000 --steps 0 1 --model MEPS --domain_name West_Norway
+#
+from weathervis.config import *
+from weathervis.utils import *
+import cartopy.crs as ccrs
+from weathervis.domain import *  # require netcdf4
+from weathervis.check_data import *
+from weathervis.get_data import *
+from weathervis.calculation import *
+import matplotlib.pyplot as plt
+import warnings
+import cartopy.feature as cfeature
+from mpl_toolkits.axes_grid1 import make_axes_locatable ##__N
+from weathervis.checkget_data_handler import *
+import gc
+
+warnings.filterwarnings("ignore", category=UserWarning) # suppress matplotlib warning
+
+def BLH(datetime, steps=[0,2], model= "MEPS", domain_name = None, domain_lonlat = None, legend=False, info = False, save = True,grid=True):
+  param = ["air_pressure_at_sea_level", "surface_geopotential","atmosphere_boundary_layer_thickness","upward_air_velocity_pl"]
+  dmet, data_domain, bad_param = checkget_data_handler( p_level=[1000], date = "2021030300", domain_name = "AromeArctic", model = model, step= steps,  all_param=param )
+  dmet.air_pressure_at_sea_level /= 100
+
+
+  plev = 0 #velocity presuure level at first request
+  coast_details='intermediate'
+  W = filter_values_over_mountain(dmet.surface_geopotential[:], dmet.upward_air_velocity_pl[:])
+  MSLP = filter_values_over_mountain(dmet.surface_geopotential[:], dmet.air_pressure_at_sea_level[:])
+  BLH = (dmet.atmosphere_boundary_layer_thickness[:, :, :]).squeeze()
+
+  crs = default_map_projection(dmet)
+  fig1, ax1 = plt.subplots(1, 1, figsize=(7, 9), subplot_kw={'projection': crs})
+
+  itim = 0
+  for leadtime in np.array(steps):
+      print('Plotting {0} + {1:02d} UTC'.format(datetime,leadtime))
+      ax1 = default_mslp_contour(dmet.x, dmet.y, MSLP[itim,:], ax1)
+
+      # vertical velocity
+      C_W = ax1.contour(dmet.x, dmet.y, W[itim,:], zorder=3, alpha=1.0,
+                         levels=np.linspace(0.07,2.0,4), colors="red", linewidths=0.7)
+      C_W = ax1.contour(dmet.x, dmet.y, W[itim,:], zorder=3, alpha=1.0,
+                        levels=np.linspace(-2.0,-0.07,4 ), colors="blue", linewidths=0.7)
+      # boundary layer thickness
+      CF_BLH = ax1.contourf(dmet.x, dmet.y, BLH[itim,:], zorder=1, alpha=0.5,
+                        levels=np.arange(500, 5000, 500), linewidths=0.7,label = "BLH", cmap = "summer",extend="both")
+      #coastline
+      ax1.add_feature(cfeature.GSHHSFeature(scale=coast_details))
+
+
+      #Done plotting, now adjusting
+      ax_cb = adjustable_colorbar_cax(fig1, ax1 )
+      fig1.colorbar(CF_BLH, fraction=0.046, pad=0.01,aspect=25,cax = ax_cb, label="Boundary layer thickness [m]", extend="both") ##__N
+
+      ax1.text(0, 1, "{0}_BLH_{1}+{2:02d}".format(model, datetime, leadtime), ha='left', va='bottom', transform=ax1.transAxes, color='dimgrey')
+      if legend:
+        llg = {'W_over':  {'color': 'red', 'linestyle': None, 'legend': f"W [m s-1]>0.07 m/s at {dmet.pressure[plev]:.0f} hPa"},
+               'W_under': {'color': 'blue', 'linestyle': 'dashed', 'legend': f"W [m s-1]<0.07 m/s at {dmet.pressure[plev]:.0f} hPa"},
+               'MSLP': {'color': 'gray', 'linestyle': None, 'legend':  "MSLP [hPa]"}  }
+        nice_legend(llg, ax1)
+      if grid:
+        nicegrid(ax=ax1)
+
+      if domain_name != model and data_domain != None:  #
+        ax1.set_extent(data_domain.lonlat)
+
+      make_modelrun_folder = setup_directory(OUTPUTPATH, "{0}".format(datetime))
+      file_path="{0}/{1}_{2}_{3}_{4}+{5:02d}.png".format(make_modelrun_folder, model, domain_name, "BLH", datetime, leadtime)
+
+      print( f"filename: {file_path}")
+      fig1.savefig(file_path, bbox_inches="tight",dpi=200)
+      ax1.cla()
+      itim +=1
+  plt.close(fig1)
+  plt.close("all")
+  del dmet
+  del data_domain
+  del bad_param
+  del crs
+  gc.collect()
+
+
+if __name__ == "__main__":
+  args = default_arguments()
+  BLH(datetime=args.datetime, steps = args.steps, model = args.model, domain_name = args.domain_name,
+          domain_lonlat=args.domain_lonlat, legend = args.legend, info = args.info,grid=args.grid)
